2906ff8bf66dff28701ed56de2e13176edc70d52
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-05-20 14:29:19 -0400

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const server = require("./server");
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 // if (process.env.NODE_ENV !== "test") {
 server.listen(port, () => {
   console.log("server running...");
